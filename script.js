b27c0ecb681e6677fdb2b3d29e266574d8227a01
rfdllh291105-crypto <rfdllh291105@gmail.com>
2026-06-19 11:52:02 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
-document.querySelectorAll('.card').forEach(card=>{
+function updateClock(){
 
-card.addEventListener('touchstart',()=>{
+const now = new Date();
 
-card.classList.add('active')
+const time = now.toLocaleTimeString('id-ID');
 
-})
+document.getElementById("clock").innerHTML = time;
 
-card.addEventListener('touchend',()=>{
+}
 
-setTimeout(()=>{
-card.classList.remove('active')
-},200)
+setInterval(updateClock,1000);
 
-})
+updateClock();
 
-})
+function scrollToAbout(){
+
+document.getElementById("about")
+.scrollIntoView({
+behavior:"smooth"
+});
+
+}
+
+ScrollReveal().reveal('.hero-content',{
+distance:'50px',
+duration:1500,
+origin:'bottom'
+});
+
+ScrollReveal().reveal('.card,.activity-card,.gallery-card,.timeline-item,.stat-card',{
+distance:'30px',
+duration:1200,
+interval:200
+});
+
+document.querySelectorAll('button,.activity-card,.gallery-card,.stat-card')
+.forEach(item=>{
+
+item.addEventListener('click',()=>{
+
+item.animate([
+{transform:'scale(1)'},
+{transform:'scale(0.95)'},
+{transform:'scale(1)'}
+],{
+duration:250
+});
+
+});
+
+});
